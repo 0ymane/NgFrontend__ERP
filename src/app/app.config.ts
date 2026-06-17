ffa97185a3691162a5918d639b7b05a6a386e49d
0ymane <kilwatest@gmail.com>
2026-06-18 00:11:55 +0100

    prime ng now

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+import Nora from '@primeuix/themes/nora';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Nora,
+        options: {
+          darkModeSelector: false
+        }
+      }
+    })
   ]
 };
